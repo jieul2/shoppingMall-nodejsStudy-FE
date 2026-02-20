1c3b817cb978edb6dc3ecb577ebc34967b4bc1a3
jieul2 <jieul2@kakao.com>
2026-02-21 01:26:24 +0900

    페이지네이션

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -7,10 +7,10 @@ export const getProductList = createAsyncThunk(
   "products/getProductList",
   async (query, { rejectWithValue }) => {
     try {
-      const response = await api.get("/product");
-      console.log("상품 목록 응답:", response.data.products);
+      const response = await api.get("/product", { params: { ...query } });
+      console.log("상품 목록 응답:", response);
       if (response.status !== 200) throw new Error("상품 목록 가져오기 실패");
-      return response.data.products || [];
+      return response.data;
     } catch (error) {
       return rejectWithValue(
         error.error || "상품 목록을 가져오는 중 오류가 발생했습니다.",
@@ -90,7 +90,7 @@ const productSlice = createSlice({
       })
       .addCase(createProduct.rejected, (state, action) => {
         state.loading = false;
-        state.error = action.payload;
+        state.error = action.payload.productList;
         state.success = false;
       })
       .addCase(getProductList.pending, (state, action) => {
@@ -98,8 +98,9 @@ const productSlice = createSlice({
       })
       .addCase(getProductList.fulfilled, (state, action) => {
         state.loading = false;
-        state.productList = action.payload;
+        state.productList = action.payload.productList;
         state.error = "";
+        state.totalPageNum = action.payload.totalPageNum;
       })
       .addCase(getProductList.rejected, (state, action) => {
         state.loading = false;
--- src/page/AdminProductPage/AdminProductPage.js
+++ src/page/AdminProductPage/AdminProductPage.js
@@ -39,11 +39,17 @@ const AdminProductPage = () => {
   //상품리스트 가져오기 (url쿼리 맞춰서)
 
   useEffect(() => {
-    dispatch(getProductList());
-  }, []);
+    dispatch(getProductList({ ...searchQuery }));
+  }, [query]);
 
   useEffect(() => {
     //검색어나 페이지가 바뀌면 url바꿔주기 (검색어또는 페이지가 바뀜 => url 바꿔줌=> url쿼리 읽어옴=> 이 쿼리값 맞춰서  상품리스트 가져오기)
+    if (searchQuery.name === "") {
+      delete searchQuery.name;
+    }
+    const params = new URLSearchParams(searchQuery);
+    const query = params.toString();
+    navigate(`?${query}`);
   }, [searchQuery]);
 
   const deleteItem = (id) => {
@@ -64,6 +70,7 @@ const AdminProductPage = () => {
 
   const handlePageClick = ({ selected }) => {
     //  쿼리에 페이지값 바꿔주기
+    setSearchQuery({ ...searchQuery, page: selected + 1 });
   };
 
   return (
@@ -91,7 +98,7 @@ const AdminProductPage = () => {
           nextLabel="next >"
           onPageChange={handlePageClick}
           pageRangeDisplayed={5}
-          pageCount={100}
+          pageCount={totalPageNum}
           forcePage={searchQuery.page - 1}
           previousLabel="< previous"
           renderOnZeroPageCount={null}
