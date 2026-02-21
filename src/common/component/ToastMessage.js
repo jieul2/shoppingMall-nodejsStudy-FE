9267e2886108a221f33eb0947378ec3a426aff37
jieul2 <jieul2@kakao.com>
2026-02-21 12:58:49 +0900

    로딩 스피너

--- src/common/component/ToastMessage.js
+++ src/common/component/ToastMessage.js
@@ -6,7 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 const ToastMessage = () => {
   const { toastMessage } = useSelector((state) => state.ui);
-  console.log("here", toastMessage);
+  console.log("ToastMessage 컴포넌트 렌더링:", toastMessage);
   useEffect(() => {
     if (toastMessage) {
       const { message, status } = toastMessage;
--- src/page/LandingPage/LandingPage.js
+++ src/page/LandingPage/LandingPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import ProductCard from "./components/ProductCard";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Spinner } from "react-bootstrap";
 import { useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductList } from "../../features/product/productSlice";
@@ -8,32 +8,40 @@ import { getProductList } from "../../features/product/productSlice";
 const LandingPage = () => {
   const dispatch = useDispatch();
 
-  const productList = useSelector((state) => state.product.productList);
+  // const productList = useSelector((state) => state.product.productList);
+  const { productList, loading } = useSelector((state) => state.product);
   const [query] = useSearchParams();
   const name = query.get("name");
   useEffect(() => {
     dispatch(
       getProductList({
         name,
-      })
+      }),
     );
   }, [query]);
 
   return (
     <Container>
       <Row>
-        {productList.length > 0 ? (
+        {/* 1. 로딩 중일 때 스피너 표시 */}
+        {loading ? (
+          <div className="text-align-center">
+            <Spinner animation="border" variant="primary" />
+          </div>
+        ) : productList.length > 0 ? (
+          /* 2. 데이터 로드 완료 및 결과가 있을 때 */
           productList.map((item) => (
             <Col md={3} sm={12} key={item._id}>
               <ProductCard item={item} />
             </Col>
           ))
         ) : (
+          /* 3. 로딩이 끝났는데 데이터가 없을 때만 '결과 없음' 표시 */
           <div className="text-align-center empty-bag">
-            {name === "" ? (
+            {name === "" || !name ? (
               <h2>등록된 상품이 없습니다!</h2>
             ) : (
-              <h2>{name}과 일치한 상품이 없습니다!`</h2>
+              <h2>{name}과 일치한 상품이 없습니다!</h2>
             )}
           </div>
         )}
